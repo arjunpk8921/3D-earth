d6383a166bb6fec4b05da0fc886f97d8a2a7f901
Arjun-pk-8921 <arjunpk8921@gmail.com>
2024-01-30 01:22:19 +0530

    changing background geometry to image

--- main.js
+++ main.js
@@ -87,10 +87,10 @@ function main() {
     scene.add(cloudMesh);
 
     //adding star geometry
-    const starGeometry = new THREE.SphereGeometry(5, 64, 64);
-    const starMaterial = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./texture/galaxy.png'), side: THREE.BackSide });
-    const starMesh = new THREE.Mesh(starGeometry, starMaterial);
-    scene.add(starMesh);
+    // const starGeometry = new THREE.SphereGeometry(5, 64, 64);
+    // const starMaterial = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./texture/galaxy.png'), side: THREE.BackSide });
+    // const starMesh = new THREE.Mesh(starGeometry, starMaterial);
+    // scene.add(starMesh);
 
     // set ambientlight
     const ambientlight = new THREE.AmbientLight(0xffffff, 0.2);
